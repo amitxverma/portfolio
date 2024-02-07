5a21f61ba63d83ecc1cbac5f5a31740ab6937c41
Amit Kumar <90851130+amitxverma@users.noreply.github.com>
2024-02-07 21:58:03 +0530

    Update BgVideo.js

--- src/components/body-bg-video/BgVideo.js
+++ src/components/body-bg-video/BgVideo.js
@@ -5,7 +5,7 @@ function BgVideo() {
 
     return (
         <>
-            <video className='container-fluid bg-video' autoplay loop muted >
+            <video className='container-fluid bg-video' muted autoplay loop  >
                 <source src={bgVideo} type="video/mp4" /> 
             </video>
         </>
