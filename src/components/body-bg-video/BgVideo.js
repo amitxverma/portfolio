4ed77c5cfe8b46338a2a1ad31c6bb0dc1f162398
Amit Kumar <90851130+amitxverma@users.noreply.github.com>
2024-02-07 21:36:04 +0530

    video issue solved2

--- src/components/body-bg-video/BgVideo.js
+++ src/components/body-bg-video/BgVideo.js
@@ -5,8 +5,8 @@ function BgVideo() {
 
     return (
         <>
-            <video className='container-fluid bg-video' autoPlay loop muted playsinline>
-                <source src={bgVideo} type="video/mp4" />
+            <video src={bgVideo} type="video/mp4" className='container-fluid bg-video' autoPlay loop muted playsinline>
+                {/* <source src={bgVideo} type="video/mp4" /> */}
             </video>
         </>
     )
